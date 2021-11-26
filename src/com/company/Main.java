1e012c47aeb3511c9c453573728657d0272c821f
VLysykh <Vladimir.Lysykh@homecredit.ru>
2021-11-26 20:27:05 +0300

    [no task] refactor

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         Stream.of(4, 8, 15, 16, -23, -42)
-                .collect(Collectors.partitioningBy(integer -> integer > 0))
-                .forEach((aBoolean, integers) -> System.out.println(aBoolean + ":" + integers.stream().mapToInt(Integer::intValue).sum()));
+                .collect(Collectors.partitioningBy(integer -> integer > 0 , Collectors.summingInt(Integer::intValue)))
+                .forEach((aBoolean, sum) -> System.out.println(aBoolean + ":" + sum));
     }
 }
